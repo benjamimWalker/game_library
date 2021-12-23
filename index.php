aaa68b66a515d792a8aa74265a24fff3c2fbbee8
benjamim <bhaskdelta2@gmail.com>
2021-12-23 18:39:11 -0300

    First version of front-end ready

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/style.css"/>
+    <title>Game Listing</title>
+</head>
+<body>
+<div id="body">
+    <h1>Choose a game</h1>
+    <table class="listing">
+        <tr><td>Image<td>Name<td>Adm
+        <tr><td>Image<td>Name<td>Adm
+        <tr><td>Image<td>Name<td>Adm
+        <tr><td>Image<td>Name<td>Adm
+        <tr><td>Image<td>Name<td>Adm
+        <tr><td>Image<td>Name<td>Adm
+    </table>
+</div>
+
+</body>
+</html>
